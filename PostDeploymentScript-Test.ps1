7572c4d4a48e0f9569b2e624180cf8a08021e735
yeswps <yeswps@yahoo.ca>
2017-06-21 08:32:33 -0400

    Add files via upload

---
+++ PostDeploymentScript-Test.ps1
@@ -0,0 +1,33 @@
+﻿$LogFile = $env:TEMP + "\" + "MITAzureVMPostDeploymentScript.Log"
+<#
+    .SYNOPSIS
+        Write to Log File
+    
+    .DESCRIPTION
+        Write to Log File
+    
+    .PARAMETER Message
+        [string] Provide message to write to log
+    
+    .EXAMPLE
+        		PS C:\> Write-Log -Message 'Important Message'
+    
+    .NOTES
+        Additional information about the function.
+#>
+function Write-Log {
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true,
+                   Position = 1,
+                   HelpMessage = '[string] Provide message to write to log')]
+        [ValidateNotNullOrEmpty()]
+        [string]$Message
+    )
+    
+    $TimeStamp = Get-Date -format "yyyy-MM-dd HH:mm:ss"
+    ($TimeStamp + " " + $Message) | Out-File -FilePath $LogFile -Append    
+}
+
+Write-Log "`n----> Log start <----"
